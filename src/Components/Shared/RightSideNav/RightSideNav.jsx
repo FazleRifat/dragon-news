68d79cf6fba23b4bb08b46ae20fea0791674e264
FazleRifat <fazlerifatghost21@gmail.com>
2023-10-03 17:20:17 +0600

    fix the title ,add login with in right side

--- src/Components/Shared/RightSideNav/RightSideNav.jsx
+++ src/Components/Shared/RightSideNav/RightSideNav.jsx
@@ -1,11 +1,25 @@
-
+import { AiFillGithub, AiOutlineGoogle } from "react-icons/ai";
 
 const RightSideNav = () => {
-    return (
-        <div>
-            <h1 className="text-2xl">RightSideNav</h1>
+  return (
+    <div>
+      <div>
+        <h1 className="text-xl font-semibold text-center md:text-left">Login with</h1>
+        <div className="border border-sky-400 rounded-lg mt-3">
+          <button className="btn text-sky-400 w-full">
+            <AiOutlineGoogle className="text-xl"></AiOutlineGoogle>Login with
+            Google
+          </button>
         </div>
-    );
+        <div className="border border-black rounded-lg mt-3">
+          <button className="btn text-black w-full">
+            <AiFillGithub className="text-xl"></AiFillGithub>Login with
+            Github
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RightSideNav;
