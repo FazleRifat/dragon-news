6ec01fbd7104eccd2246fb8b83a3dcc8fe26634c
FazleRifat <fazlerifatghost21@gmail.com>
2023-10-03 18:18:40 +0600

    category done (on the left side)

--- src/Components/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Components/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const LeftSideNav = () => {
+    const [categories,setCategory] =useState([])
+    useEffect(()=>{
+        fetch('categories.json')
+        .then(res => res.json())
+        .then(data => setCategory(data))
+    },[])
+    console.log(categories);
     return (
         <div>
-            <h1 className="text-2xl">LeftSideNav</h1>
+            <h1 className="text-2xl">All Caterogy</h1>
+            <ul>
+            {
+                categories.map(category =><li key={category.id} className="text-xl font-semibold ml-4 mt-5 active:bg-base-300 p-3 rounded-md"><Link>{category.name}</Link></li> )
+            }
+            </ul>
         </div>
     );
 };
