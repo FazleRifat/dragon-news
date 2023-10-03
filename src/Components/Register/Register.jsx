e0a269af8f790fb405ab0f5f6fcc8b622f714cef
FazleRifat <fazlerifatghost21@gmail.com>
2023-10-03 23:13:03 +0600

    register page created

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
+
 
 const Register = () => {
   return (
@@ -14,8 +16,23 @@ const Register = () => {
             </h2>
             <hr className="mx-auto w-96" />
             <form className="card-body ml-14 mr-20">
+            <div className="form-control">
+                <label className="label" htmlFor="name">
+                  <span className="label-text font-semibold">
+                    Your Name
+                  </span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter your name"
+                  className="input input-bordered bg-base-200"
+                  id="name"
+                  name="name"
+                  required
+                />
+              </div>
               <div className="form-control">
-                <label className="label" htmlFor="emal">
+                <label className="label" htmlFor="email">
                   <span className="label-text font-semibold">
                     Email address
                   </span>
@@ -30,7 +47,7 @@ const Register = () => {
                 />
               </div>
               <div className="form-control">
-                <label className="label">
+                <label className="label" htmlFor="password">
                   <span className="label-text font-semibold">Password</span>
                 </label>
                 <input
@@ -41,14 +58,11 @@ const Register = () => {
                   id="password"
                   required
                 />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
-                </label>
+                <div className="flex mt-3 gap-2"><input type="checkbox" name="terms" id="terms" required />
+                <label htmlFor="terms">Accept <Link className="text-neutral-700 font-semibold">Terms & policy</Link></label></div>
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-neutral">Login</button>
+                <button className="btn btn-neutral">Register</button>
               </div>
             </form>
           </div>
